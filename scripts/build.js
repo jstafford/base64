af212b78ccc0ae794036001ff4f9054915970788
Jason Stafford <jason.stafford@scrycollective.com>
2017-02-19 05:02:16 -0800

    switch all tabs to spaces

--- scripts/build.js
+++ scripts/build.js
@@ -4,19 +4,19 @@ var template = require('lodash.template');
 
 // http://whatwg.org/html/common-microsyntaxes.html#space-character
 var spaceCharacters = regenerate(
-	0x0020, // U+0020 SPACE
-	0x0009, // U+0009 CHARACTER TABULATION (tab)
-	0x000A, // U+000A LINE FEED (LF)
-	0x000C, // U+000C FORM FEED (FF)
-	0x000D  // U+000D CARRIAGE RETURN (CR)
+  0x0020, // U+0020 SPACE
+  0x0009, // U+0009 CHARACTER TABULATION (tab)
+  0x000A, // U+000A LINE FEED (LF)
+  0x000C, // U+000C FORM FEED (FF)
+  0x000D  // U+000D CARRIAGE RETURN (CR)
 );
 
 var packageStr = fs.readFileSync('package.json', 'utf-8');
 var packageJson = JSON.parse(packageStr);
 
 var data = {
-	'spaceCharacters': spaceCharacters.toString(),
-	'version': packageJson.version
+  'spaceCharacters': spaceCharacters.toString(),
+  'version': packageJson.version
 };
 
 // the following based on code from gulp-template by Sindre Sorhus at
